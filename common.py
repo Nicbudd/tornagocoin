3e7fac1f05647a1f8ab7b8b48535ff85065862f3
nicbudd <nicbudd@nicbudd.com>
2025-02-02 12:11:04 -0500

    updated requirements

--- common.py
+++ common.py
@@ -8,7 +8,3 @@ ADMIN = 396730242460418058
 
 def tornago(ctx):
     return discord.utils.get(ctx.bot.emojis, name="tornago")
-
-async def author_color(ctx):
-    user = await ctx.bot.fetch_user(ctx.author.id)
-    return user.accent_color
